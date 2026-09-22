5b502bd11dfd3bb11f3e1efc236b6d41ddd9761c
Naufal Arif <na713634@gmail.com>
2026-09-22 11:04:08 +0700

    perbaikan

--- FindLargesNumber.py
+++ FindLargesNumber.py
@@ -2,11 +2,11 @@ angka1 = float(input("Masukkan angka pertama: "))
 angka2 = float(input("Masukkan angka kedua: "))
 angka3 = float(input("Masukkan angka ketiga: "))
 
-if angka1 >= angka2 and angka1 >= angka3:
-    terbesar = angka1
-elif angka2 >= angka1 and angka2 >= angka3:
-    terbesar = angka2
+if angka1 > angka2 and angka1 > angka3:
+    print("Angka terbesar adalah:", angka1)
+elif angka2 > angka1 and angka2 > angka3:
+    print("Angka terbesar adalah:", angka2)
+elif angka3 > angka1 and angka3 > angka2:
+    print("Angka terbesar adalah:", angka3)
 else:
-    terbesar = angka3
-
-print("Angka terbesar adalah:", terbesar)
+    print("Terdapat angka yang sama atau tidak ada angka terbesar.")
